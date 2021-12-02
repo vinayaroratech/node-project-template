c2a4107428026f712510a93c1e07a4bfa24dcef0
Vinay Arora <vin.aroar@gmail.com>
2021-12-02 22:58:04 +0530

    Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: [
+      2,
+      'always'
+    ]
+  }
+};
